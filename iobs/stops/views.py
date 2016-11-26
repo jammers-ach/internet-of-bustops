0ade3aaa9a3efcc5c405b531eacb3642ab58cf9e
jammers <jammers@gmail.com>
2016-11-26 10:29:54 +0200

    Load bus stops for sessions

--- iobs/stops/views.py
+++ iobs/stops/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import BusStop
 
-# Create your views here.
+def start_view(request):
+    # get the buss stop associated with this session
+    if 'stop_id' not in request.session:
+        count = BusStop.objects.filter().count()
+        stop = BusStop.objects.create(name="Bus stop {}".format(count),
+                              lat=0,
+                              lng=0)
+        stop.save()
+        request.session['stop_id'] = stop.id
+    else:
+        stop = BusStop.objects.get(id=request.session['stop_id'])
+
+
+    return HttpResponse('You are stop: {}'.format(stop))
+
